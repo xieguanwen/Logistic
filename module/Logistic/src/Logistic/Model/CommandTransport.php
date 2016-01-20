d797a360d9b7448e43d0955fdee8cd2fe8c47696
rich <xiegw@xiaolajiao.com>
2016-01-20 11:28:50 +0800

    修改物流

--- module/Logistic/src/Logistic/Model/CommandTransport.php
+++ module/Logistic/src/Logistic/Model/CommandTransport.php
@@ -43,7 +43,7 @@ class CommandTransport {
      */
     public function receiveOrder(OrderInfo $orderInfo){
         $response = $this->sender->sendParam($this->receiver->receiveOrderData($orderInfo),'ecerp.sendorder.get','GET');
-        print_r($response->getBody());exit();
+//        print_r($response->getBody());exit();
         $logisticNo = $this->xml->getWldh($response->getBody());
         if($logisticNo){
             $result = $logisticNo;
