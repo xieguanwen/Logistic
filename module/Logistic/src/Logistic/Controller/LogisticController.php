9e86277ca38248d850e0bd3797e7cdb0228b1861
rich <xiegw@xiaolajiao.com>
2016-05-09 15:00:38 +0800

    单个订单

--- module/Logistic/src/Logistic/Controller/LogisticController.php
+++ module/Logistic/src/Logistic/Controller/LogisticController.php
@@ -95,6 +95,7 @@ class LogisticController extends AbstractActionController {
         $orderInfoTable = $this->getServiceLocator()->get('Order\Model\OrderInfoTable');
         $orderGoodsTable = $this->getServiceLocator()->get('Order\Model\OrderGoodsTable');
         if ($this->getRequest()->getQuery('order_sn')) {
+            print_r(array('order_sn'=>$this->getRequest()->getQuery('order_sn')));exit;
             $orderInfo = $orderInfoTable->fetchOne(array('order_sn'=>$this->getRequest()->getQuery('order_sn')));
             $result = $this->commandTransport->sendOrder($orderInfo, $orderGoodsTable);
             print_r($this->commandTransport->getResponse()->getBody());
