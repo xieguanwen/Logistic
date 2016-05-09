22f4eada4e6c59ebb056bf79b8d017ab6c129ac9
rich <xiegw@xiaolajiao.com>
2016-05-09 15:02:43 +0800

    测试

--- module/Logistic/src/Logistic/Controller/LogisticController.php
+++ module/Logistic/src/Logistic/Controller/LogisticController.php
@@ -95,8 +95,9 @@ class LogisticController extends AbstractActionController {
         $orderInfoTable = $this->getServiceLocator()->get('Order\Model\OrderInfoTable');
         $orderGoodsTable = $this->getServiceLocator()->get('Order\Model\OrderGoodsTable');
         if ($this->getRequest()->getQuery('order_sn')) {
-            print_r(array('order_sn'=>$this->getRequest()->getQuery('order_sn')));exit;
+            print_r(array('order_sn'=>$this->getRequest()->getQuery('order_sn')));
             $orderInfo = $orderInfoTable->fetchOne(array('order_sn'=>$this->getRequest()->getQuery('order_sn')));
+            print_r($orderInfo);exit;
             $result = $this->commandTransport->sendOrder($orderInfo, $orderGoodsTable);
             print_r($this->commandTransport->getResponse()->getBody());
             if ($result) {
