8d860e6198c320d8cb2ade8b92534ea647b56c4a
rich <xiegw@xiaolajiao.com>
2016-10-31 11:37:31 +0800

    修改测试

--- module/Logistic/src/Logistic/Model/Sender.php
+++ module/Logistic/src/Logistic/Model/Sender.php
@@ -68,6 +68,7 @@ class Sender {
         } else {
             $this->client->setParameterGet($param);
         }
+        print_r($this->client->getUri()->toString());exit;
 //        print_r($this->client->getRequest()->getQuery());exit;
         $response = $this->client->send();
         return $response;
