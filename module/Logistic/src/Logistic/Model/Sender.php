461bb26cfb220bc1a7c2c1609d20f3468ceeea79
rich <xiegw@xiaolajiao.com>
2016-11-14 14:47:50 +0800

    测试接收

--- module/Logistic/src/Logistic/Model/Sender.php
+++ module/Logistic/src/Logistic/Model/Sender.php
@@ -73,6 +73,7 @@ class Sender {
         } else {
             $this->client->setParameterGet($param);
         }
+        print_r($this->client->getUri());
         print_r($this->client->getRequest()->getQuery());exit;
         $response = $this->client->send();
         return $response;
